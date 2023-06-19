b06c45612953f8655adf6fa1bb2d4b2c11c5109e
hito-de-jiro <lollexuss@gmail.com>
2023-06-19 16:00:35 +0300

    add: added and setting test environment to config.py

--- config.py
+++ config.py
@@ -3,11 +3,34 @@ from flask import Flask
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a really really really really long secret key'
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:root27@localhost/library'  # connect database
+DB_PATH = 'mysql+pymysql://root:root27@localhost/library'  # connect database
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_PATH
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = DB_PATH
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+class DevelopmentConfig(Config):
+    ENV = "development"
+    DEVELOPMENT = True
+    SQLALCHEMY_DATABASE_URI = DB_PATH
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+class TestingConfig(DevelopmentConfig):
+    TESTING = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = DB_PATH
