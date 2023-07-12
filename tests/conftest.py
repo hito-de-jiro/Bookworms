3295d161ae672a65b3a14170733b56a1ea8c74a2
hito-de-jiro <lollexuss@gmail.com>
2023-07-12 22:49:50 +0300

    add: continuing work on tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,14 +2,15 @@ import pytest
 from bookworm import config
 
 from bookworm.config import db
-from bookworm.app import app
+from bookworm.app import create_app
 from bookworm.models.models import Author
 
 
 @pytest.fixture
 def client():
+    app = create_app()
     app.config.from_object(config.TestingConfig)
-    with app.test_client():
+    with app.test_client() as client:
         with app.app_context():
             yield client
 
@@ -19,9 +20,9 @@ def init_database():
     db.create_all()
 
     test_authors = [
-        {"first_name": "Francois ", "last_name": "Villon", "dob": "1431-04-01", "books": []},
-        {"first_name": "Victor", "last_name": "Hugo", "dob": "1802-02-26", "books": []},
-        {"first_name": "William", "last_name": "Shakespeare", "dob": "1564-04-26", "books": []},
+        {"first_name": "Francois ", "last_name": "Villon", "borne": "1431-04-01", "books": []},
+        {"first_name": "Victor", "last_name": "Hugo", "borne": "1802-02-26", "books": []},
+        {"first_name": "William", "last_name": "Shakespeare", "borne": "1564-04-26", "books": []},
     ]
 
     def create_post_model(author):
--- tests/test_api.py
+++ tests/test_api.py
@@ -2,14 +2,15 @@ def test_add_new_author(client, init_database):
     path = "api/v1/authors"
     response = client.post(path,
                            json={
+                               "id": 5,
                                "first_name": "Alexandre",
                                "last_name": "Dumas",
-                               "dob": "1802-07-24",
+                               "borne": "1802-07-24",
                                "books": []
                            })
 
     assert response.status_code == 200
-    assert b'{"firs_name": "Alexander", "last_name": "Dumas", "dob": "1823-01-14"}' in response.data
+    assert b'{"id": 5, "firs_name": "Alexander", "last_name": "Dumas", "borne": "1823-01-14"}' in response.data
 
 
 def test_get_all_authors(client, init_database):
@@ -17,3 +18,8 @@ def test_get_all_authors(client, init_database):
     response = client.get(path)
 
     assert b'[{}]' in response.data
+
+
+def test_empty_db(client, init_database):
+    rv = client.get('/')
+    assert b'No entries here so far' in rv.data
