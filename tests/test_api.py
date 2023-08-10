d0a752712fc4fe78e9e2430409b6384f512429f6
hito-de-jiro <lollexuss@gmail.com>
2023-08-10 11:41:59 +0300

    fix: test for authors moved to test_authors.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,32 +1,22 @@
-def test_add_new_author(client, init_database):
-    path = "api/v1/authors"
-    response = client.post(path,
-                           json={
-                               "id": 1,
-                               "first_name": "Alexandre",
-                               "last_name": "Dumas",
-                               "borne": "1802-07-24",
-                               "books": []
-                           })
+import json
 
+from bookworm.app import create_app
+from bookworm import config
+
+
+def test_create_app():
+    assert create_app()
+
+
+def test_config():
+    assert config.TestingConfig
+
+
+def test_client(client):
+    response = client.get('/')
     assert response.status_code == 200
-    # assert b'{"id": 5, "firs_name": "Alexander", "last_name": "Dumas", "borne": "1823-01-14"}' in response.data
-    data = response.json()
-    print(data)
 
 
-def test_update_author():
-    # TODO document why this method is empty
-    pass
 
 
-def test_get_all_authors(client, init_database):
-    path = "api/v1/authors"
-    response = client.get(path)
 
-    assert b'[{}]' in response.data
-
-
-def test_empty_db(client, init_database):
-    rv = client.get('/')
-    assert b'No entries here so far' in rv.data
