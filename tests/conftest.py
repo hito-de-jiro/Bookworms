332ad3d4c39a0308e372e6bfd3954a228da7de57
hito-de-jiro <lollexuss@gmail.com>
2023-08-10 11:38:59 +0300

    fix: modified conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import pytest
-from bookworm import config
 
-from bookworm.config import db
+from bookworm import config
 from bookworm.app import create_app
-from bookworm.models.models import Author
+from bookworm.config import db
+from bookworm.models.models import Author, Book
 
 
 @pytest.fixture
@@ -25,16 +25,41 @@ def init_database():
         {"first_name": "William", "last_name": "Shakespeare", "borne": "1564-04-26", "books": []},
     ]
 
-    def create_post_model(author):
+    test_book = [
+        {
+            "title": "Test title",
+            "text": "Test text",
+            "genre": "Test genre",
+        }
+    ]
+
+    def create_author_model(author):
         return Author(**author)
 
-    mapped_authors = map(create_post_model, test_authors)
+    def create_book_model(book):
+        return Book(**book)
+
+    mapped_authors = map(create_author_model, test_authors)
     t_authors = list(mapped_authors)
 
+    mapped_books = map(create_book_model, test_book)
+    t_book = list(mapped_books)
+
     db.session.add_all(t_authors)
+    db.session.add_all(t_book)
     db.session.commit()
 
     yield db
 
+    db.session.remove()
+    db.drop_all()
+
+
+@pytest.fixture
+def test_database():
+    """fixture for cleaning and leveraging test db"""
+
+    db.create_all()
+    yield db  # testing happens here
     db.session.remove()
     db.drop_all()
