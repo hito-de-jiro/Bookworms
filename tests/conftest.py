7f78ffdaa9fd8e5ebfc709d9734c22ff675c1fca
hito-de-jiro <lollexuss@gmail.com>
2023-08-16 11:23:03 +0300

    add: added books schema for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 from bookworm.app import create_app
 from bookworm.config import TestingConfig
-from bookworm.models.models import Author, db
+from bookworm.models.models import Author, Book, db
 
 
 @pytest.fixture
@@ -23,13 +23,25 @@ def init_database(client):
         {"id": 3, "first_name": "William", "last_name": "Shakespeare", "borne": "1564-04-26"},
     ]
 
+    test_books = [
+        {"id": 1, "title": "Ballade des pendus", "text": "Frères humains, qui ...", "genre": "Balada", "author_id": 1},
+        {"id": 2, "title": "Some title", "text": "Some text", "genre": "Drama", "author_id": 2},
+        {"id": 3, "title": "Some title again", "text": "Very interesting text", "genre": "Adventures", "author_id": 3},
+    ]
+
     def create_author_model(author):
         return Author(**author)
 
+    def create_book_model(book):
+        return Book(**book)
+
     mapped_authors = map(create_author_model, test_authors)
     t_authors = list(mapped_authors)
+    mapped_books = map(create_book_model, test_books)
+    t_books = list(mapped_books)
 
     db.session.add_all(t_authors)
+    db.session.add_all(t_books)
     db.session.commit()
 
     yield db
